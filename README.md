aaa2808d52632e9b5f05adf2ac56b96a59e74e49
bret.k.ikehara <bret.k.ikehara@gmail.com>
2012-09-25 00:06:11 -1000

    Refactored the sensor code.
    Added the Travis CI status to the readme.md.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # wattdepot-visualization
 
-Further the development of energy visualization driven by the Wattdepot service. 
+Further the development of energy visualization. 
+
+## Build status
+
+[![Build Status](https://secure.travis-ci.org/kiyomasa84/wattdepot-visualization.png)](http://travis-ci.org/kiyomasa84/wattdepot-visualization)
 
 ## Repository setup
 
 This repository has been subdivided into two portions. One is for the processing mockups and another for the wattdepot-visualizations.
-
--- src/main/java/org/wattdepot/visualization/page/home/Sensor.js
+++ src/main/java/org/wattdepot/visualization/page/home/Sensor.js
@@ -1,13 +1,16 @@
+/**
+ * Creates the WattDepot sensor animation module.
+ * 
+ * @author Bret K. Ikehara
+ */
 YUI().add('wattdepot-sensor', function(Y) {
   Y.namespace("WattDepot");
+  
   /**
-   * Creates the WattDepot sensor.
+   * Defines the WattDepot sensor.
    */
   var Sensor = function() {
-
-    // define the functions
-    var init, drawFunc, updateFunc, sendFunc, isAnimingFunc;
-    // define the fields.
+    // define the private fields.
     var circle, P;
 
     // creates the object.
@@ -30,86 +33,121 @@ YUI().add('wattdepot-sensor', function(Y) {
     circle.color.val = circle.color.def;
 
     /**
-     * Initiates this sensor.
-     * 
-     * @param processing
-     *          Processing Object.
-     */
-    init = function(processing) {
-      P = processing;
-    };
-
-    /**
-     * Draws the sensor.
-     */
-    drawFunc = function() {
-      var c, fill;
-
-      c = P.color(circle.color.val, 0, 0);
-      fill = circle.radius.max - circle.radius.val;
-      P.stroke(c, fill);
-      P.fill(c, fill);
-      P.ellipse(56, 46, circle.radius.val, circle.radius.val);
-
-      alpha = 255;
-      P.stroke(c, alpha);
-      P.fill(c, alpha);
-      P.ellipse(56, 46, circle.radius.def, circle.radius.def);
-    };
-
-    /**
-     * Updates the sensor.
-     */
-    updateFunc = function() {
-      var endFade = false, endBlink = false;
-
-      // animate when data was sent.
-      if (circle.isAnim) {
-        endBlink = (circle.color.val < circle.color.min);
-        endFade = (circle.radius.val > circle.radius.max);
-        if (!endFade) {
-          circle.radius.val += circle.fade.o;
-        }
-
-        if (!endBlink) {
-          circle.color.val -= circle.fade.i;
-        }
-
-        // end animation when both are done.
-        if (endFade && endBlink) {
-          circle.isAnim = false;
-        }
-      }
-    };
-
-    /**
-     * Sends the data.
-     */
-    sendFunc = function() {
-      circle.isAnim = true;
-      circle.radius.val = circle.radius.def;
-      circle.color.val = circle.color.def;
-    };
-
-    /**
-     * Gets whether the sensor is being animated.
-     */
-    isAnimateFunc = function() {
-      return circle.isAnim;
-    };
-
-    /**
-     * List of all public methods.
+     * Defines the public functions.
      */
     return {
-      init : init,
-      draw : drawFunc,
-      update : updateFunc,
-      isAnimate : isAnimateFunc,
-      send : sendFunc
+      /**
+       * Sets this radius.
+       * 
+       * @param r
+       *          Integer value.
+       */
+      setRadius : function(r) {
+        circle.radius.def = r;
+      },
+
+      /**
+       * Sets this max radius.
+       * 
+       * @param r
+       *          Integer value.
+       */
+      setRadiusMax : function(r) {
+        circle.radius.max = r;
+      },
+
+      /**
+       * Sets this outer fade speed.
+       * 
+       * @param f
+       *          Integer
+       */
+      setFadeOuter : function(f) {
+        circle.fade.o = f;
+      },
+
+      /**
+       * Sets this outer fade speed.
+       * 
+       * @param f
+       *          Integer
+       */
+      setFadeInner : function(f) {
+        circle.fade.i = f;
+      },
+
+      /**
+       * Initiates this sensor.
+       * 
+       * @param processing
+       *          Processing Object.
+       */
+      init : function(processing) {
+        P = processing;
+      },
+
+      /**
+       * Draws the sensor.
+       */
+      draw : function() {
+        var c, fill;
+
+        c = P.color(circle.color.val, 0, 0);
+        fill = circle.radius.max - circle.radius.val;
+        P.stroke(c, fill);
+        P.fill(c, fill);
+        P.ellipse(56, 46, circle.radius.val, circle.radius.val);
+
+        alpha = 255;
+        P.stroke(c, alpha);
+        P.fill(c, alpha);
+        P.ellipse(56, 46, circle.radius.def, circle.radius.def);
+      },
+
+      /**
+       * Updates the sensor.
+       */
+      update : function() {
+        var endFade = false, endBlink = false;
+
+        // animate when data was sent.
+        if (circle.isAnim) {
+          endBlink = (circle.color.val < circle.color.min);
+          endFade = (circle.radius.val > circle.radius.max);
+          if (!endFade) {
+            circle.radius.val += circle.fade.o;
+          }
+
+          if (!endBlink) {
+            circle.color.val -= circle.fade.i;
+          }
+
+          // end animation when both are done.
+          if (endFade && endBlink) {
+            circle.isAnim = false;
+          }
+        }
+      },
+
+      /**
+       * Sends the data.
+       */
+      send : function() {
+        circle.isAnim = true;
+        circle.radius.val = circle.radius.def;
+        circle.color.val = circle.color.def;
+      },
+
+      /**
+       * Gets whether the sensor is being animated.
+       */
+      isAnimate : function() {
+        return circle.isAnim;
+      }
     };
   };
 
+  // Define object in global space.
   Y.WattDepot.Sensor = Sensor;
 }, '1.0.0', {
   requires : [ 'node-base' ]
