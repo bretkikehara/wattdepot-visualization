95003162f0719e0078c4bf1306ccc27e297d383d
bret.k.ikehara <bret.k.ikehara@gmail.com>
2012-09-25 14:20:15 -1000

    Updated the documentation.

--- src/main/java/org/wattdepot/visualization/page/home/home.js
+++ src/main/java/org/wattdepot/visualization/page/home/home.js
@@ -1,3 +1,8 @@
+/**
+ * Creates the Processing canvas for the home page.
+ * 
+ * @author Bret K. Ikehara
+ */
 YUI().use('node', 'wattdepot-sensor', function(Y) {
   var processing;
   (function() {
@@ -6,24 +11,30 @@ YUI().use('node', 'wattdepot-sensor', function(Y) {
      * Sketches the processing app.
      */
     sketchProc = function(P) {
-      
       var sensor, sendTimer, sendTimerMax;
-      
+
+      /**
+       * Sets up the processing canvas.
+       */
       P.setup = function() {
         P.size(500, 500);
-        
+
         // create the sensor
-        sensor  = new Y.WattDepot.Sensor();
+        sensor = new Y.WattDepot.Sensor();
         sensor.init(P);
-        
+
         // emulate sending data on a time interval.
         sendTimerMax = 50;
         sendTimer = sendTimerMax;
       };
-      
+
+      /**
+       * Handles drawing the Processing canvas.
+       */
       P.draw = function() {
         P.background(255);
-        
+
+        // mocks data being sent at an interval.
         if (sendTimer > 0 && !sensor.isAnimate()) {
           sendTimer -= 1;
         }
@@ -31,10 +42,10 @@ YUI().use('node', 'wattdepot-sensor', function(Y) {
           sensor.send();
           sendTimer = sendTimerMax;
         }
-        
+
         // draws all items
         sensor.draw();
-        
+
         // updates all items.
         sensor.update();
       };
@@ -44,6 +55,7 @@ YUI().use('node', 'wattdepot-sensor', function(Y) {
     canvas = Y.Node.create('<canvas/>');
     canvas.appendTo('div#bd');
 
+    // initiates the processing object.
     processing = new Processing(canvas.getDOMNode(), sketchProc);
   }());
 });
