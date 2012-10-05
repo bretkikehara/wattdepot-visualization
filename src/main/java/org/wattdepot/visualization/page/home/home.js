2ac6e5a98c8fd8972f22439b12b2c6e852d36e66
bret.k.ikehara <bret.k.ikehara@gmail.com>
2012-10-04 20:47:36 -1000

    Merged from test-branch

--- src/main/java/org/wattdepot/visualization/page/home/home.js
+++ src/main/java/org/wattdepot/visualization/page/home/home.js
@@ -6,51 +6,53 @@ YUI().use('node', 'wattdepot-sensor', 'wattdepot-transmission', function(Y) {
      * Sketches the processing app.
      */
     sketchProc = function(P) {
-      
-      var sensor, sendTimer, sendTimerMax, transmission;
-      
+      var sensor, sensors, sendTimer, sendTimerMax, transmission;
+
       P.setup = function() {
         P.size(600, 400);
-        
-        //PImage background;
-        //background = P.loadImage("UHmap.png");
-        //image(background, 0, 0);
-        
+
+        // PImage background;
+        // background = P.loadImage("UHmap.png");
+        // image(background, 0, 0);
+
         // create the sensor
-        sensor  = new Y.WattDepot.Sensor();
-        sensor.init(P);
-        
+        sensor = new Y.WattDepot.Sensor(P, {
+          color : [ 255, 0, 0, 0 ],
+          colorHandler : function(P, o) {
+            o.color[0] -= 2;
+          },
+          x : 400 * Math.random(),
+          y : 400 * Math.random()
+        });
+
         // create the transmission
         transmission = new Y.WattDepot.Transmission(400, 400, 100, 100);
         transmission.init(P);
-        
+
         // emulate sending data on a time interval.
         sendTimerMax = 50;
         sendTimer = sendTimerMax;
       };
-      
+
       P.draw = function() {
         P.background(255);
-        
+
         if (sendTimer > 0 && !sensor.isAnimate()) {
           sendTimer -= 1;
         }
         else if (sendTimer == 0) {
-          sensor.send();
+          sensor.animate();
+          transmission.sendTransmission();
           sendTimer = sendTimerMax;
         }
-        
+
         // draws all items
         sensor.draw();
-        
+        transmission.draw();
+
         // updates all items.
         sensor.update();
-        
-        // draws the edge
-        transmission.draw();
-        transmission.sendTransmission();
         transmission.transmissionAnim();
-        
       };
     };
 
